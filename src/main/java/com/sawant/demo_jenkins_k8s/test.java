1f5a3f6fe4552a6d98ce1d22c19fce62bb4a9cb6
vsawantvinay <vsawant.azure@gmail.com>
2026-03-20 21:26:28 +0530

    added missing text

--- src/main/java/com/sawant/demo_jenkins_k8s/test.java
+++ src/main/java/com/sawant/demo_jenkins_k8s/test.java
@@ -1,5 +1,8 @@
 package com.sawant.demo_jenkins_k8s;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class test {
